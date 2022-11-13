94ff31e2321c9b9736abbc77a48d0c98e0dd340d
Espen Hellerud <espenhellerud5@gmail.com>
2022-11-13 11:51:24 +0100

    Set up netlify hosting

--- web-ssr/remix.config.js
+++ web-ssr/remix.config.js
@@ -1,8 +1,10 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  ignoredRouteFiles: ["**/.*"],
+  serverBuildTarget: 'netlify',
+  server: process.env.NETLIFY || process.env.NETLIFY_LOCAL ? './server.js' : undefined,
+  ignoredRouteFiles: ['**/.*'],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
-  // serverBuildPath: "build/index.js",
+  // serverBuildPath: ".netlify/functions-internal/server.js",
   // publicPath: "/build/",
 };
