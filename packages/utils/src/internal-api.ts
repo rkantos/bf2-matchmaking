167add6c1235aa9df794f5e0f6737605e606b48c
Espen Hellerud <espenhellerud5@gmail.com>
2023-02-02 19:34:50 +0100

    Add headers to internal api

--- packages/utils/src/internal-api.ts
+++ packages/utils/src/internal-api.ts
@@ -4,7 +4,11 @@ export const bot = () => {
   const basePath = 'https://bot.bf2-matchmaking-production.up.railway.app';
   return {
     postMatchEvent: async (matchId: number, event: MatchEvent) => {
+      const headers = {
+        'Content-Type': 'application/json',
+      };
       const res = await fetch(`${basePath}/api/match_events`, {
+        headers,
         method: 'POST',
         body: JSON.stringify({ matchId, event }),
       });
